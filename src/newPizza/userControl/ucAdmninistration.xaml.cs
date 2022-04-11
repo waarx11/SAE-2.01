33f3fc12b61c1a1291c82d2ef2fcdd4ca040cc25
Nathan Verdier <nathan.verdier@etu.uca.fr>
2022-04-11 09:59:58 +0200

    uc administrateur

---
+++ src/newPizza/userControl/ucAdmninistration.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace newPizza
+{
+    /// <summary>
+    /// Logique d'interaction pour ucAdmninistration.xaml
+    /// </summary>
+    public partial class ucAdmninistration : UserControl
+    {
+        public ucAdmninistration()
+        {
+            InitializeComponent();
+        }
+
+        public string Message
+        {
+            set
+            {
+                nomEntreprise.Text = value;
+            }
+        }
+    }
+}
