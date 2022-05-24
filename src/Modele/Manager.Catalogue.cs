6f0d4dd2069491e3b5cad6006dd7bbae7a42c271
Nathan Verdier <nathan.verdier@etu.uca.fr>
2022-05-24 15:56:04 +0200

    Manager bien avancer

---
+++ src/Modele/Manager.Catalogue.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace Modele
+{
+    public partial class Manager
+    {
+        public readonly Catalogue Catalogues;
+        public Catalogue C1 = new Catalogue();
+
+        public bool AjouteCatalogueVegetarien(Pizza pizz)
+        {
+            if (pizz == null)
+            {
+                return false;
+            }
+
+            if (C1.CatalogueVegetarien.Contains(pizz))
+            {
+                return false;
+            }
+            C1.CatalogueVegetarien.Add(pizz);
+            return true;
+        }
+
+
+        public bool AjouteCataloguePizzeta(Pizza pizz)
+        {
+            if (pizz == null)
+            {
+                return false;
+            }
+
+            if (C1.CataloguePizzeta.Contains(pizz))
+            {
+                return false;
+            }
+            C1.CataloguePizzeta.Add(pizz);
+            return true;
+        }
+
+        public bool AjouteCatalogueCarnivore(Pizza pizz)
+        {
+            if (pizz == null)
+            {
+                return false;
+            }
+
+            if (C1.CatalogueCarnivore.Contains(pizz))
+            {
+                return false;
+            }
+            C1.CatalogueCarnivore.Add(pizz);
+            return true;
+        }
+
+        public bool AjouteCatalogueEpice(Pizza pizz)
+        {
+            if (pizz == null)
+            {
+                return false;
+            }
+
+            if (C1.CatalogueEpice.Contains(pizz))
+            {
+                return false;
+            }
+            C1.CatalogueEpice.Add(pizz);
+            return true;
+        }
+
+        public bool SuppCatalogueVegetarien(Pizza pizz)
+        {
+            if (pizz == null)
+            {
+                return false;
+            }
+
+            if (C1.CatalogueVegetarien.Contains(pizz))
+            {
+                return false;
+            }
+            C1.CatalogueVegetarien.Remove(pizz);
+            return true;
+        }
+
+
+        public bool SuppCataloguePizzeta(Pizza pizz)
+        {
+            if (pizz == null)
+            {
+                return false;
+            }
+
+            if (C1.CataloguePizzeta.Contains(pizz))
+            {
+                return false;
+            }
+            C1.CataloguePizzeta.Remove(pizz);
+            return true;
+        }
+
+        public bool SuppCatalogueCarnivore(Pizza pizz)
+        {
+            if (pizz == null)
+            {
+                return false;
+            }
+
+            if (C1.CatalogueCarnivore.Contains(pizz))
+            {
+                return false;
+            }
+            C1.CatalogueCarnivore.Remove(pizz);
+            return true;
+        }
+
+        public bool SuppCatalogueEpice(Pizza pizz)
+        {
+            if (pizz == null)
+            {
+                return false;
+            }
+
+            if (C1.CatalogueEpice.Contains(pizz))
+            {
+                return false;
+            }
+            C1.CatalogueEpice.Remove(pizz);
+            return true;
+        }
+
+        public Catalogue GetCatalogue(Catalogue c1)
+        {
+            return Catalogues.SingleOrDefault(a => a.Equals(c1));
+        }
+
+        public Administrateur ModifieCatalogue(Catalogue adOld, Catalogue adNew)
+        {
+            return null;
+        }
+    }
+}
--- src/Modele/Manager.cs
+++ src/Modele/Manager.cs
@@ -18,12 +18,14 @@ namespace Modele
             //Persist = p;
             Administrateurs = new ReadOnlyCollection<Administrateur>(administrateurs);
             Clients = new ReadOnlyCollection<Client>(clients);
+            Catalogues = C1;
         }
 
         public void ChargeDonnées()
         {
-            Administrateur A1 = new Administrateur("Bonneau", "Baptiste", "Baptiste@oui.com", "09.25.25.73.19", "27 rue non", "Clermont-Ferrand", "63000", "nojii");
+            Administrateur A1 = new Administrateur("Peroni", "Luigi", "Luigi.PERONI@NewPizza.com", "09.25.25.80.18", "4 Avenue BlaiseMcool", "Clermont-Ferrand", "63000", "NewPizza()");
             ChargeClient();
+            ChargeCatalogue();
         }
 
         public void ChargeClient()
@@ -31,8 +33,40 @@ namespace Modele
             clients.AddRange(new Client[]
             {
                 new Client("Kartal", "Emre", "emre@oui.com", "06.45.85.95.15", "27 rue oui", "Clermont-Ferrand", "63100", "tobiii", "non"),
-                new Client("Verdier", "Nathan", "Baptiste@oui.com", "09.25.25.73.19", "27 rue non", "Clermont-Ferrand", "63000", "nojii"),
+                new Client("Verdier", "Nathan", "Nathan@oui.com", "09.25.25.73.19", "27 rue non", "Clermont-Ferrand", "63000", "Waarx"),
+                new Client("Bonneau", "Baptiste", "Baptiste@oui.com", "09.25.25.73.19", "27 rue non", "Clermont-Ferrand", "63000", "nojji", "img"),
         });
+        }
+
+        public void ChargeCatalogue()
+        {
+            C1.CatalogueVegetarien.AddRange(new Pizza[]
+            {
+                new Pizza("Reine", "Bonne", "oui", new List<Ingredients> { (Ingredients)2, (Ingredients)20 } , 15),
+                new Pizza("tkt", "non", "oui", new List<Ingredients> { (Ingredients)55, (Ingredients)13, (Ingredients)42, (Ingredients)21, (Ingredients)36 }, 102),
+                new Pizza("Saucisse", "gourmande", "non", new List<Ingredients> { (Ingredients)2, (Ingredients)20, (Ingredients)35 }),
+            });
+
+            C1.CataloguePizzeta.AddRange(new Pizza[]
+            {
+                new Pizza("Reine", "Bonne", "oui", new List<Ingredients> { (Ingredients)2, (Ingredients)20 } , 15),
+                new Pizza("tkt", "non", "oui", new List<Ingredients> { (Ingredients)55, (Ingredients)13, (Ingredients)42, (Ingredients)21, (Ingredients)36 }, 102),
+                new Pizza("Saucisse", "gourmande", "non", new List<Ingredients> { (Ingredients)2, (Ingredients)20, (Ingredients)35 }),
+            });
+
+            C1.CatalogueCarnivore.AddRange(new Pizza[]
+            {
+                new Pizza("Reine", "Bonne", "oui", new List<Ingredients> { (Ingredients)2, (Ingredients)20 } , 15),
+                new Pizza("tkt", "non", "oui", new List<Ingredients> { (Ingredients)55, (Ingredients)13, (Ingredients)42, (Ingredients)21, (Ingredients)36 }, 102),
+                new Pizza("Saucisse", "gourmande", "non", new List<Ingredients> { (Ingredients)2, (Ingredients)20, (Ingredients)35 }),
+            });
+
+            C1.CatalogueEpice.AddRange(new Pizza[]
+            {
+                new Pizza("Reine", "Bonne", "oui", new List<Ingredients> { (Ingredients)2, (Ingredients)20 } , 15),
+                new Pizza("tkt", "non", "oui", new List<Ingredients> { (Ingredients)55, (Ingredients)13, (Ingredients)42, (Ingredients)21, (Ingredients)36 }, 102),
+                new Pizza("Saucisse", "gourmande", "non", new List<Ingredients> { (Ingredients)2, (Ingredients)20, (Ingredients)35 }),
+            });
         }
     }
 }
