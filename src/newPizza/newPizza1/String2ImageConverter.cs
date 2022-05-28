48f59ec9b03393246a154f43a5abe8f750470845
Baptiste | Nojii_ <baptiste.Tab4@gmail.com>
2022-05-28 16:01:36 +0200

    string to image converter & edit path images dans le manager

---
+++ src/newPizza/newPizza1/String2ImageConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+
+namespace newPizza1.converters
+{
+    class String2ImageConverter : IValueConverter
+    {
+        private static string imagesPath;
+
+
+        static String2ImageConverter()
+        {
+            imagesPath = Path.Combine(Directory.GetCurrentDirectory(), "..\\img\\");
+        }
+
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string imageName = value as string;
+
+            if (string.IsNullOrWhiteSpace(imageName)) return new Uri("..\\default\\noImg.png", UriKind.RelativeOrAbsolute);
+
+            string imagePath = Path.Combine(imagesPath, imageName);
+
+            return new Uri(imagePath, UriKind.RelativeOrAbsolute);
+
+        }
+
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+
+}
