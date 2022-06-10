04fe8f7df76335848f05833eaddfe3d73372052d
naverdier <Nathan.VERDIER@etu.uca.fr>
2022-06-10 19:24:31 +0200

    avant dernier push

---
+++ src/TestsUnitaires/UT_Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Modele;
+using System.Collections.Generic;
+using Xunit;
+
+namespace TestsUnitaires
+{
+    public class UT_Manager
+    {
+        
+        [Fact]
+        public void TEST_ChargeDonne()
+        {
+
+        }
+
+
+    }
+}
